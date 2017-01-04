2d8259231b8fc666d81e84ad14b333d10cda9136
Andres Jimenez <andresdavid@outlook.com>
2017-01-04 16:17:41 -0400

    release 1.0.0-alpha.3

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @class ModelRegister
+ * @author Jonathan Casarrubias <t: johncasarrubias, gh: mean-expert-official>
+ * @license MIT
+ * @description
+ * This class will register a model reference, hooks and remote methods.
+ */
+export declare class ModelRegister {
+    constructor(Model: any, reference: any);
+}
--- lib/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './src/model-register';
